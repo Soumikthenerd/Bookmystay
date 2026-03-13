813eb0c1079edee0ceda1ca311c4acfdb9fce613
Soumik <soumikm778@gmail.com>
2026-03-13 09:02:09 +0530

    Add files via upload

---
+++ uc2.java
@@ -0,0 +1,24 @@
+public abstract class Room {
+
+    /** Number of beds available in the room. */
+    protected int numberOfBeds;
+
+    /** Total size of the room in square feet. */
+    protected int squareFeet;
+
+    /** Price charged per night for this room type. */
+    protected double pricePerNight;
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    /** Displays room details. */
+    public void displayRoomDetails() {
+        System.out.println("Room Details:");
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq ft");
+        System.out.println("Price per night: $" + pricePerNight);
+    }
+}
