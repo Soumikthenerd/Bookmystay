7e05ac5dc98c97cde13a72a30806861fc47a4dd4
Soumik <soumikm778@gmail.com>
2026-03-13 09:55:11 +0530

    Add files via upload

---
+++ uc11.java
@@ -0,0 +1,68 @@
+/**
+ * ==============================================================
+ * MAIN CLASS - UseCase11ConcurrentBookingSimulation
+ * ==============================================================
+ *
+ * Use Case 11: Concurrent Booking Simulation
+ *
+ * Description:
+ * This class simulates multiple users
+ * attempting to book rooms at the same time.
+ *
+ * It highlights race conditions and
+ * demonstrates how synchronization
+ * prevents inconsistent allocations.
+ *
+ * @version 11.0
+ */
+
+public class UseCase11ConcurrentBookingSimulation {
+
+    /**
+     * Application entry point.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Concurrent Booking Simulation");
+
+        RoomInventory inventory = new RoomInventory();
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+        RoomAllocationService allocationService = new RoomAllocationService();
+
+        // Add booking requests
+        bookingQueue.addRequest(new Reservation("Abhi", "Single"));
+        bookingQueue.addRequest(new Reservation("Vanmathi", "Double"));
+        bookingQueue.addRequest(new Reservation("Kural", "Suite"));
+        bookingQueue.addRequest(new Reservation("Subha", "Single"));
+
+        // Create booking processors
+        Thread t1 = new Thread(
+                new ConcurrentBookingProcessor(
+                        bookingQueue, inventory, allocationService
+                )
+        );
+
+        Thread t2 = new Thread(
+                new ConcurrentBookingProcessor(
+                        bookingQueue, inventory, allocationService
+                )
+        );
+
+        // Start threads
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Thread execution interrupted.");
+        }
+
+        // Print remaining inventory
+        System.out.println("\nRemaining Inventory:");
+        System.out.println("Single: " + inventory.getRoomAvailability().get("Single"));
+        System.out.println("Double: " + inventory.getRoomAvailability().get("Double"));
+        System.out.println("Suite: " + inventory.getRoomAvailability().get("Suite"));
+    }
+}
